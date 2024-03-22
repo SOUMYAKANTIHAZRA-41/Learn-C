ec8de3050cb59a659046df00016e0a78572c3b7f
soumyakantihazra-41 <soumyakantihazra2003@gmail.com>
2024-03-22 18:59:16 +0530

    3rd code

---
+++ codechef/course/LBCL01_problems_CLB004.c
@@ -0,0 +1,18 @@
+// Task
+// Try to add 21 and 40 in code and print the result. Remember, we don't need "" (double quotes) when printing numbers.
+
+// Comments
+// Notice that on the editor on the right, the instructions appear in gray color.
+// This is because they are comments and don't affect the code.
+// The way we write comments is by adding "//" at the start of a line.
+
+
+#include <stdio.h>
+
+int main() {
+
+  // Replace the underscores
+  printf("%d",21+40);
+return 0;
+}
+gi
